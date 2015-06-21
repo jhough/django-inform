96802bfc3655c04dcc63b69f1e2c52439b79393a
Jim Hough <jim@jimhough.com>
2015-06-21 12:49:15 -0400

    removed commented out import statements

--- inform/polls/urls.py
+++ inform/polls/urls.py
@@ -1,16 +1,6 @@
-#from django.conf.urls import patterns, url
-from .views import PollsListView, PollsDetailView, PollsResultsView, vote
-
-#from __future__ import absolute_import, unicode_literals
 from django.conf.urls import url
-#from . import views
 
-#urlpatterns = patterns('',
-#    url(r'^$', IndexView.as_view(), name='index'),
-#    url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
-#    url(r'^(?P<pk>\d+)/results/$', ResultsView.as_view(), name='results'),
-#    url(r'^(?P<poll_id>\d+)/vote/$', vote, name='vote'),
-#)
+from .views import PollsListView, PollsDetailView, PollsResultsView, vote
 
 urlpatterns = [
     url(regex=r'^$', view=PollsListView.as_view(), name='list'),
